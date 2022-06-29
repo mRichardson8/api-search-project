149f611e72ad51b0e6561eb35524a387f1b00502
Matthew Richardson <matt.rich2045@gmail.com>
2022-06-29 14:34:56 +0100

    Added up to 10 dummy websites

--- server/server.js
+++ server/server.js
@@ -3,11 +3,15 @@ const app = express();
 const cors = require('cors');
 const ejs = require('ejs')
 
+app.use(cors());
+
+app.set('view engine','ejs')
+app.use(express.static(__dirname + '/public'))
+
 const search=[{
     name:'Youtube',
     url:'http://www.youtube.com',
     description:'videos',
-
 },{
     name:'GAME',
     url:'http://www.game.co.uk',
@@ -15,8 +19,44 @@ const search=[{
 },{
     name:'primark',
     url:'http://www.primark.com',
-    description:'retail store',
-}]
+    description:'Cheap clothes',
+},
+{
+    name:'JD Sports',
+    url:'http://www.jdsports.co.uk',
+    description:'Sports clothes',
+},
+{
+    name:'Sports Direct',
+    url:'http://www.sports-direct.com',
+    description:'Even more sports clothes',
+},
+{
+    name:'Tesco',
+    url:'http://www.Tesco.co.uk',
+    description:'Every little helps',
+},
+{
+    name:'Sainsburys',
+    url:'http://www.sainsburys.com',
+    description:`We're one class higher than tesco's`,
+},
+{
+    name:'Cineworld',
+    url:'http://www.cineworld.com',
+    description:'Movies at way too high a price',
+},
+{
+    name:'WHSmiths',
+    url:'http://www.whsmiths.co.uk',
+    description:'The best store',
+},
+{
+    name:'Greggs',
+    url:'http://www.greggs.co.uk',
+    description:'Vegan sausage rolls are pretty good',
+}
+]
 
 
 
@@ -26,10 +66,7 @@ app.get(('/search') , (req,res) => {
 
 
 
-app.use(cors());
 
-app.set('view engine','ejs')
-app.use(express.static(__dirname + '/public'))
 
 app.get(('/test'),(req,res)=>{
     res.render('page',{details:search[0]})
