b21f541edf1daad593dd34d267c7af654ef686dc
HassanNaeem <hassannaeem288@gmail.com>
2022-06-29 12:52:05 +0100

    changes done

--- client/assets/api.js
+++ client/assets/api.js
@@ -3,15 +3,36 @@ let getResultBtn = document.querySelector('#get-result')
 
 
 resultsBtn.addEventListener('click' , (e) =>{
-    fetch('http://localhost:3000/')
-        .then(res => res.text())
-        .then(res => console.log(res))
+    fetch('http://localhost:3000/search')
+        .then(res => res.json())
+        .then(data => {
+            for(i=0;i<data.length;i++){
+                displayObject(data[i])
+
+            }
+        })
+        
+        // .then(res => res.forEach(element => {
+        //     console.log(element);
+            
+        // }))
 })
 
 getResultBtn.addEventListener('click' , (e) =>{
-    fetch('http://localhost:3000/')
-        .then(res => res.text())
-        .then(res => console.log(res))
+    window.location.href = "http://localhost:3000/test";
 })
 
 
+
+function displayObject(data){
+    let sectionSearch=document.getElementById('Search')
+    let name= document.createElement('h3')
+    let url =document.createElement('p')
+    let description= document.createElement('p')
+    name.innerText=data.name
+    url.innerText=data.url
+    description.innerText=data.description
+    sectionSearch.appendChild(name)
+    sectionSearch.appendChild(url)
+    sectionSearch.appendChild(description)
+}
--- server/server.js
+++ server/server.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const ejs = require('ejs')
+
+const search=[{
+    name:'Youtube',
+    url:'http://www.youtube.com',
+    description:'videos',
+
+},{
+    name:'GAME',
+    url:'http://www.game.co.uk',
+    description:'retail store',
+},{
+    name:'primark',
+    url:'http://www.primark.com',
+    description:'retail store',
+}]
+
+
+
+app.get(('/search') , (req,res) => {
+    res.send(search)
+})
+
+
 
 app.use(cors());
+
+app.set('view engine','ejs')
+
+app.get(('/test'),(req,res)=>{
+    res.render('page',{details:search[0]})
+})
+
+
 
 app.get(('/') , (req,res) => {
     res.send('Hello World!')
 })
 
 app.listen(3000);
+
